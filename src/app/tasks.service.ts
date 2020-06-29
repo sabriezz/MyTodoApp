a0cbf59425cbd6e15498782db4721815dfdb548a
sabriezz <31842350+sabriezz@users.noreply.github.com>
2020-06-29 18:17:30 +0200

    Bug Fixed

--- src/app/tasks.service.ts
+++ src/app/tasks.service.ts
@@ -2,30 +2,27 @@ import {TaskComponent} from "./task/task.component";
 export class TasksService{
 
   getDoing() : TaskComponent[]{
-    let v = new TaskComponent();
     let doing: Array<TaskComponent> =[];
-    v.taskTitle="esempio";
-    v.taskDate="13/23/21"
-    doing.push(v);
-    doing.push(v);
-    doing.push(v);
-    doing.push(v);
+    if (JSON.parse(localStorage.getItem('doing'))!==null){
+      doing=JSON.parse(localStorage.getItem('doing'));
+    }
     return doing;
   }
   getDone() : TaskComponent[]{
-    let v = new TaskComponent();
     let done : Array<TaskComponent> =[];
-    done.push(v);
+
+    if (JSON.parse(localStorage.getItem('done'))!==null){
+      done=JSON.parse(localStorage.getItem('done'));
+    }
     return done;
   }
 
   getToDo() : TaskComponent[]{
     let todo : Array<TaskComponent> =[];
+    if (JSON.parse(localStorage.getItem('todo'))!==null){
+      todo=JSON.parse(localStorage.getItem('todo'));
+    }
     return todo;
   }
 
-  addTask(task: TaskComponent) {
-
-  }
 }
-
--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -16,6 +16,13 @@ export class TasksComponent {
   @Input() doing : Array<TaskComponent> =[];
   @Input() done : Array<TaskComponent> =[];
 
+
+  update(){
+    localStorage.setItem('todo', JSON.stringify(this.todo));
+    localStorage.setItem('doing', JSON.stringify(this.doing));
+    localStorage.setItem('done', JSON.stringify(this.done));
+  }
+
   drop(event: CdkDragDrop<TaskComponent[]>) {
     if (event.previousContainer === event.container) {
       moveItemInArray<TaskComponent>(event.container.data, event.previousIndex, event.currentIndex);
@@ -25,8 +32,23 @@ export class TasksComponent {
         event.previousIndex,
         event.currentIndex);
     }
+    this.update();
   }
 
+  delete(i: number,array:number){
+    if(array===0){
+        this.todo.splice(i, 1);
+    }else if(array===1){
+        this.doing.splice(i, 1);
+    }else{
+        this.done.splice(i,1);
+    }
+    this.update();
+  }
+
+  modify(task: TaskComponent){
+    console.log(task);
+  }
 }
 
 
